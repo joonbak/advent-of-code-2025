103e774d46ea1c7630fe555a9ac64e7a1cc91070
Joon Bak <jbdev98@gmail.com>
2025-12-06 12:11:48 +1100

    Solve Day 3 Part 2

--- day-3/2.py
+++ day-3/2.py
@@ -1,21 +1,20 @@
 with open("input.txt", "r") as file:
-    lines = [line.strip() for line in file]
+    lines = [[int(x) for x in line.strip()] for line in file]
 
 sum = 0
 
 for line in lines:
-    best = 0
-    stack = []
+    best_list = []
+    best_idx = 0
 
-    for i in range(len(line)):
-        while (len(stack) > 0 and stack[-1] < line[i] and len(stack) + (len(line) - i) > 12):
-            stack.pop()
-        stack.append(line[i])
+    for i in range(11, -1, -1):
+        for j in range(best_idx, len(line) - i):
+            if line[j] > line[best_idx]:
+                best_idx = j
+        best_list.append(line[best_idx])
+        best_idx += 1
 
-        while len(stack) > 12:
-            stack.pop()
-
-    best = "".join(stack)
-    sum += int(best)
+    joined = "".join(map(str, best_list))
+    sum += int(joined)
 
 print(sum)
