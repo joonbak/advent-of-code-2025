bc4c487c9f919ebae2aa5d109b9fe6b86d789219
Joon Bak <jbdev98@gmail.com>
2025-12-09 22:34:39 +1100

    Solve Day 8 Part 2

--- day-8/2.py
+++ day-8/2.py
@@ -3,7 +3,6 @@ with open("input.txt", "r") as file:
 
 distances = []
 
-
 for i, (x1, y1, z1) in enumerate(points):
     for j, (x2, y2, z2) in enumerate(points):
         if i > j:
@@ -26,49 +25,15 @@ def merge(a: int, b: int):
     parent[find(a)] = find(b)
 
 
-for a, b, _ in distances[:1000]:
+circuits = len(points)
+
+for a, b, _ in distances:
+    if find(a) == find(b):
+        continue
+
     merge(a, b)
 
-sizes = [0] * len(points)
-
-for box in range(len(points)):
-    sizes[find(box)] += 1
-
-sizes.sort(reverse=True)
-
-print(sizes[0] * sizes[1] * sizes[2])
-
-
-"""
-conn = {}
-for i, (x1, y1, z1) in enumerate(coords[:-1]):
-    for j, (x2, y2, z2) in enumerate(coords[i + 1 :], start=i + 1):
-        sum = (x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2
-        distance = sum**0.5
-        conn_coords = str(coords[i]) + ":" + str(coords[j])
-        conn[conn_coords] = distance
-        # print(conn_coords)
-
-sorted_dist = sorted(conn.items(), key=lambda item: item[1])
-# for i in range(10):
-#    print(sorted_dist[i])
-res = []
-cnt = 0
-for k, v in sorted_dist:
-    co1, co2 = k.split(":")
-    set1 = {co1, co2}
-    if len(res) == 0:
-        res.append(set1)
-    for i in range(len(res)):
-        if res[i].intersection(set1):
-            res[i] = res[i].union(set1)
-        else:
-            res.append(set1)
+    circuits -= 1
+    if circuits == 1:
+        print(points[a][0] * points[b][0])
         break
-    if cnt == 10:
-        break
-    cnt += 1
-
-for i in res:
-    print(i)
-"""
